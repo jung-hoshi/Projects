1719dbd5a5c7c1fa4da7f02f965cc1a6a0d1ec8b
jung-hoshi <k.pikulik@abcmobile.com>
2023-10-19 22:40:16 +0300

    add new project

---
+++ app/js/main.js
@@ -0,0 +1,46 @@
+function slowScroll(id) {
+	$("html, body").animate({
+		scrollTop: $(id).offset().top - 50
+	}, 500);
+	return false;
+}
+
+$(".header-top .menu").on("click", function() {
+	if($("header .mobile-menu").is(":visible"))
+	  $(this).html('<div class="menu"><img src="img/menu.png" alt="">');
+	else
+	  $(this).html('<div class="menu"><img src="img/menu.png" alt="">');
+  
+	$("header .mobile-menu").slideToggle();
+  });
+
+$("#subscribe").on("click", function() {
+	let email = $("#email").val();
+	email = email.trim();
+	if(email.split("@").length != 2 || email.split(".").length != 2) {
+	  $("#sub_form label").text("Wrong Email");
+	  $("#sub_form label").fadeIn();
+	}
+  
+	setTimeout(function() {
+	  $("#sub_form label").fadeOut();
+	}, 1500);
+  });
+
+
+$('.video-play, #modal-video .close-button').on('click', function() {
+	$("#modal-video").toggle();
+	$("body").toggleClass("overflow-hidden");
+	resizeVideo();
+  });
+  
+  $(window).on('resize', function() {
+	resizeVideo();
+  }).resize();
+  
+  function resizeVideo() {
+	$("iframe").each(function() {
+	  let width = $(this).width();
+	  $(this).css("height", width / 1.77 + "px");
+	});
+  }
